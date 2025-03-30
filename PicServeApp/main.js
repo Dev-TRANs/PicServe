455c07186a5e681f9ec94062d7f8d0dbf464c8a7
forelps <dev.forelps@gmail.com>
2025-03-30 12:32:00 +0900

    Fix main.js

--- PicServeApp/main.js
+++ PicServeApp/main.js
@@ -21,7 +21,7 @@ function doPost(e) {
     const file = folder.createFile(blob);
     
     const fileId = file.getId();
-    const imageUrl = "	https://lh3.googleusercontent.com/d/" + fileId;
+    const imageUrl = "https://lh3.googleusercontent.com/d/" + fileId;
     
     return ContentService.createTextOutput(JSON.stringify({
       success: true,
